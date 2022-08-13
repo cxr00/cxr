17eff8f04322c49f983fcb2e63ba6f40d4dbd1b9
Joshua Conrad <jozbornn@gmail.com>
2022-08-13 06:43:34 -0700

    Final commit
    I consider my endeavor on this project complete - each base contains at least 13 terms, and my computer deserves a rest. I will focus on assessing the viability of adding some or all of these sequences to the OEIS.

--- tests/base36/choix_de_bruxelles.py
+++ tests/base36/choix_de_bruxelles.py
@@ -9,7 +9,7 @@ import threading
 This module explores the Choix de Bruxelles algorithm in bases between
 2 and 36. You can learn about this algorithm here: https://www.youtube.com/watch?v=AeqK96UX3rA
 
-When run, this script will compute CDB based on the given threshold, and output
+When run, this script will compute CDB and output
 each result to its appropriate location. At the end of computation, the main script
 will display variants of http://oeis.org/A323289 for each base.
 
@@ -20,10 +20,6 @@ Same goes for four-timesing, etc.
 """
 
 output_dir = "cdb"  # output files are of the form {output_dir}\\cdb{base}\\{step}.txt
-
-# This is the largest set size that CDBN will attempt to operate on
-# It can be optionally modified at the beginning of the main function
-threshold = 100
 
 
 def choix_de_bruxelles(d, length, log=False):
@@ -48,13 +44,13 @@ def choix_de_bruxelles(d, length, log=False):
                 quotient = quotient.integer_part()
                 to_add = s_d[:i] + str(quotient) + s_d[i + substring_length:]
                 td = Td.get_from_string(to_add)
-                # if td not in output:
-                output.add(td)
+                if td not in output:
+                    output.add(td)
             # Can always double selected substring
             to_add = s_d[:i] + str(i_ss * two) + s_d[i + substring_length:]
             td = Td.get_from_string(to_add)
-            # if td not in output:
-            output.add(td)
+            if td not in output:
+                output.add(td)
 
     if not isinstance(d, list):
         d = [d]
@@ -149,24 +145,22 @@ def compute_next_cdb_in_base(base):
     l_g = len(g)
     print(f"Difference yields set of size {l_g} for base {base}")
 
-    if l_g <= threshold:
-        # Perform CDBN if consistent with threshold
-        max_length = max([len(k.integer) for k in g]) + 1
-        g = choix_de_bruxelles(g, max_length, log=True)
+    # Perform CDBN
+    max_length = max([len(k.integer) for k in g]) + 1
+    g = choix_de_bruxelles(g, max_length, log=True)
 
-        # Recombine with previous step to get full set
-        g = set(g).union(s)
-        g = list(sorted(g))
+    # Recombine with previous step to get full output set
+    g = set(g).union(s)
+    g = list(sorted(g))
 
-        os.makedirs(f"{output_dir}\\cdb{base}", exist_ok=True)
-        with open(f"{output_dir}\\cdb{base}\\{current_step}.txt", "w+") as f:
-            for each in g:
-                f.write(str(each) + "\n")
+    os.makedirs(f"{output_dir}\\cdb{base}", exist_ok=True)
+    with open(f"{output_dir}\\cdb{base}\\{current_step}.txt", "w+") as f:
+        for each in g:
+            f.write(str(each) + "\n")
 
-        print(f"Step {current_step} of base {base} saved.")
+    print(f"Step {current_step} of base {base} saved.")
 
-        return True
-    return False
+    return True
 
 
 def main():
@@ -174,24 +168,30 @@ def main():
     Selects opportunities to compute CDB based on the given threshold.
 
     At the end, prints all potential OEIS sequences
-    """
-    global threshold
 
-    the_range = range(2, 37)  # Alter this (to a list even) to change what bases are examined
+    You can modify the_range to tackle a particular base or set of bases.
+    For example, the_range = [7] will only compute base 7,
+    while the_range = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31] computes all viable prime bases
+    """
+
+    the_range = range(2, 37)
     trivial = [2 ** n for n in range(6)]
 
-    while True:
+    compute = False  # False means show results, True means compute
+    inp = "continue"
+    while inp.lower() == "continue":
+        print(f"Beginning CDB analysis of {the_range}...")
         sequences = {}
 
         # Iterate over all bases
         for base in the_range:
             # Bases of the form 2**n are trivial, so are skipped
             if base not in trivial:
-                compute_next_cdb_in_base(base)
+                if compute:
+                    compute_next_cdb_in_base(base)
                 sequences[base] = analyze_cdb(base)
         print()
 
-        threshold = -1
         # Displays the sequences associated with each base
         for base in the_range:
             if base not in trivial:
@@ -202,10 +202,7 @@ def main():
             for k, v in sequences.items():
                 f.write(f"{k}:{str(v)}\n")
         print("sequences.txt updated.")
-
-        lst = sorted(list(sequences.values()), key=lambda x: x[-1])
-        threshold = lst[len(lst) // 2][-1]  # Set new threshold in the middle of what's available
-        print(f"New threshold of {threshold} assigned.")
+        inp = input("Cycle complete. Type 'continue' to go again, or simply press ENTER to quit")
 
 
 if __name__ == "__main__":
