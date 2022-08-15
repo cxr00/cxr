05267774a3459f55c57d72d5c214dae91e5169a6
Joshua Conrad <jozbornn@gmail.com>
2022-08-15 16:56:00 -0700

    Include std_l in imports

--- cxr/__init__.py
+++ cxr/__init__.py
@@ -1,6 +1,6 @@
 from cxr.math import Td
 from cxr.math import Htd
-from cxr.math import Seq, Sig, Matrix, Prism, x
+from cxr.math import Seq, Sig, Matrix, Prism, x, std_l
 from cxr.math import base36, htd
 
 from cxr.state import SM, SMR
