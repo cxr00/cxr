056b6836a1384177f88a56255eab25c6f571a21a
Joshua Conrad <jozbornn@gmail.com>
2022-09-07 18:48:17 -0700

    prismatic conv vs SDP

--- tests/snr/tests01.py
+++ tests/snr/tests01.py
@@ -1,4 +1,5 @@
-from cxr import Seq, Matrix
+from cxr import Seq, Matrix, Prism, SDP
+import time
 
 
 def test_4_5_identities():
@@ -35,6 +36,30 @@ def test_4_5_identities():
 
     print((a + b).f())
     print(Seq(1, 1).f(seed=Seq(2, 2)))
+
+
+def prismatic_convolution_vs_sdp():
+    d = Seq(1, 1)
+    n = 3
+    l = 13
+
+    t = time.time()
+    p = Prism.canonical([d] * n, l=l)
+    # p, count = Prism.canonical([d]*n, l=l)
+    pf = p.f([d] * (n + 1))
+    pi = pf.i()
+    # print(count)
+    print()
+    print(pf)
+    print(pi)
+    print(time.time() - t)
+    print()
+
+    t = time.time()
+    a = SDP([d] * n, [d] * n)
+    print(len(a))
+    print(a)
+    print(time.time() - t)
 
 
 def main():
