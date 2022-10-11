af57d601de2edafd4a512d758ac1b9f632bec611
Joshua Conrad <jozbornn@gmail.com>
2022-10-11 15:56:48 -0700

    Slightly improved error message

--- cxr/sociomathematics/clq.py
+++ cxr/sociomathematics/clq.py
@@ -125,9 +125,9 @@ class Clq:
                     output[i+1] = bc[output[i+1]]  # set - iso: f = f'
                 else:
                     try:
-                        k = 1 + funcs.index(bc[ac[i+1]]) * 2  # find r'f''
+                        k = 1 + funcs.index(bc[ac[i+1]]) * 2  # find r'
                     except ValueError as exc:
-                        raise UndefinedError(f"The solution to {a} / {b} is undefined; cannot reduce {output[i+1]} to {bc[output[i+1]]}")
+                        raise UndefinedError(f"The solution to {a} / {b} is undefined; {bc[output[i+1]]} does not exist to remove so {output[i+1]} can reduce")
                     output[k], output[k+1] = "-", "-" # red - remove: r'f''
                     if output[i] != "-":  # Only assign to roles that haven't been removed
                         output[i+1] = bc[ac[i+1]]  # red - set: f = r'
