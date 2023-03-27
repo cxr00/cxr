6620d9e63c3f92630ba648abd84556518ce4e641
Joshua Conrad <jozbornn@gmail.com>
2023-03-26 19:18:14 -0700

    Added catalan triangles and hankel matrix

--- tests/snr/triangles01.py
+++ tests/snr/triangles01.py
@@ -92,6 +92,68 @@ def t8():
     print()
 
 
+def catalan_triangles():
+    l = set_std_l(18)
+
+    def catalan():
+        output = Seq(1)
+        for i in range(1, l):
+            output = output.f(l=i+1)
+        return output
+
+    j = 3
+    a = 2
+    m = Matrix.blank()
+    m[0][0] = 1
+    c = catalan()
+
+    for n in range(1, l):
+        for k in range(n+1):
+            m[n][k] = m[n-1][k-1] + j * m[n-1][k+1]
+
+    print(m.f(a=a))
+    # print(m.i(a=a))
+
+    r_j = Seq([c[n] * j**(n+1) for n in range(l)])
+    alt = x**a + x * r_j.aerate(2)
+    print(alt.f())
+    print()
+
+    m = Matrix.blank()
+
+    for n in range(l):
+        m[n][0] = 1
+        m[n][1] = n
+        for k in range(2, n+1):
+            m[n][k] = m[n][k-1] + m[n-1][k]
+
+    print(m.i(a=a))
+
+    alt = 1 + x**a * catalan().aerate(a+1)
+    print(alt)
+    print()
+
+
+def hankel():
+    l = set_std_l(40)
+    d = Seq(1, 1)
+    f_d = d.f(l*2)
+
+    m = Matrix([f_d[n:n+l] for n in range(l)])
+
+    for a in range(1, 6):
+        print(m.i(a=a))
+
+        alt = Seq(1)
+        for k in range(1, len(d)):
+            alt += d[k-1] * k * x**(a*k+1)
+
+        aer = d.aerate(a) * x**(a-1)
+        alt *= (d.sig() + aer).f().seq
+        print(alt.i()[:l-1])
+        print()
+
+
 if __name__ == "__main__":
     t1()
     t2()
@@ -101,3 +163,5 @@ if __name__ == "__main__":
     t6()
     t7()
     t8()
+    catalan_triangles()
+    hankel()
