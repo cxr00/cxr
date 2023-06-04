7fe6fbec7c89e6255aa646cd14b9b44ce6a4b9ef
Joshua Conrad <jozbornn@gmail.com>
2023-06-04 15:24:57 -0700

    Import random_seq and random_matrix
    Also fixed import of simplex and g_matrix identities

--- cxr/__init__.py
+++ cxr/__init__.py
@@ -2,6 +2,7 @@ from cxr.math import Complex
 from cxr.math import Td
 from cxr.math import Htd
 from cxr.math import Seq, Sig, Matrix, Prism, x, std_l, set_std_l, SDP
+from cxr.math import random_seq, random_matrix, g_prism_identity, simplex_identity
 from cxr.math import base64, set_chars64, htd
 
 from cxr.colloquiality import Clq, InvalidStringError, UndefinedError
--- cxr/math/__init__.py
+++ cxr/math/__init__.py
@@ -3,6 +3,8 @@ from cxr.math.base64 import set_chars64
 from cxr.math.complex import Complex
 from cxr.math.htd import Htd
 
-from cxr.math.snr import Seq, Sig, Matrix, Prism, x, std_l, set_std_l, signature_dot_product, g_prism_identity, simplex_identity
+from cxr.math.snr import Seq, Sig, Matrix, Prism, x, std_l,\
+    set_std_l, signature_dot_product, g_prism_identity, simplex_identity,\
+    random_seq, random_matrix
 
 SDP = signature_dot_product
